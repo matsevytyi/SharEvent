e77c7640ca0530198213d1da4e7152745a9856ba
andriimatsevytyi <macevitiya@i.ua>
2023-10-23 15:47:05 -0400

    initial commit. created an interactive map

---
+++ src/main/java/com/example/mapsdemo/CustomWaypoint.java
@@ -0,0 +1,12 @@
+package com.example.mapsdemo;
+
+import org.jdesktop.swingx.mapviewer.DefaultWaypoint;
+
+public class CustomWaypoint extends DefaultWaypoint {
+
+
+
+    public CustomWaypoint(double latitude, double longitude) {
+        super(latitude, longitude);
+    }
+}
--- src/main/java/com/example/mapsdemo/HelloApplication.java
+++ src/main/java/com/example/mapsdemo/HelloApplication.java
@@ -7,11 +7,15 @@ import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import org.jdesktop.swingx.JXMapKit;
+import org.jdesktop.swingx.JXMapViewer;
+import org.jdesktop.swingx.mapviewer.*;
 
 import javax.swing.*;
 import java.io.IOException;
-
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 
 public class HelloApplication extends Application {
@@ -32,9 +36,29 @@ public class HelloApplication extends Application {
 
     private void createSwingContent(final SwingNode swingNode) {
         SwingUtilities.invokeLater(() -> {
+
+            double latitude = 43.651070;
+            double longitude = -79.347015;
+
             JXMapKit mapKit = new JXMapKit();
             mapKit.setDefaultProvider(JXMapKit.DefaultProviders.OpenStreetMaps);
 
+            mapKit.setZoom(4);
+            mapKit.setAddressLocation(new GeoPosition(latitude, longitude));
+
+            // Create a list of waypoints (markers)
+            Set<Waypoint> waypoints = new HashSet<>();
+            DefaultWaypoint restaurantMarker = new DefaultWaypoint(44.651575, -78.345010);
+            waypoints.add(restaurantMarker);
+            waypoints.add(new DefaultWaypoint(43.651070, -79.347015));
+
+            // Create a WaypointPainter to display the markers
+            WaypointPainter<Waypoint> waypointPainter = new WaypointPainter<Waypoint>();
+            waypointPainter.setWaypoints(waypoints);
+
+            // Set the WaypointPainter for the map
+            JXMapViewer mapViewer = mapKit.getMainMap();
+            mapViewer.setOverlayPainter(waypointPainter);
 
             // Create a JavaFX SwingNode to host the map component
             swingNode.setContent(mapKit);
@@ -45,15 +69,5 @@ public class HelloApplication extends Application {
         launch(args);
     }
 }
-/*
 
-    JFrame frame = new JFrame();
-    JXMapKit kit = new JXMapKit();
-       kit.setDefaultProvider(JXMapKit.DefaultProviders.OpenStreetMaps);
-               JPanel parentPanel = new JPanel();
-               parentPanel.setLayout(new BorderLayout());
-               parentPanel.add(kit, BorderLayout.CENTER);
-               frame.setContentPane(parentPanel);
-               frame.pack();
-               frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-               frame.setVisible(true);*/
+
