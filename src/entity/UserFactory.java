d132d8ad0cce710bdce74c67fd98c1de3997649f
goahead-lk-04 <55618525+goahead-lk-04@users.noreply.github.com>
2023-11-26 22:37:08 +0200

    Delete src/entity directory

--- src/entity/UserFactory.java
+++
@@ -1,9 +0,0 @@
-package ENTITY;
-
-import java.time.LocalDateTime;
-
-// just to create new user object
-public interface UserFactory {
-    /** Requires: password is valid. */
-    User create(String username, String name, String email, String password);
-}
--- src/entity/UserFactoryImplementation.java
+++
@@ -1,9 +0,0 @@
-package ENTITY;
-
-
-public class UserFactoryImplementation implements UserFactory{
-    @Override
-    public User create(String username, String name, String email, String password) {
-        return new User(username, name, email, password, null, null);
-    }
-}
