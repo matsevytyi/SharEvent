a05837c39d73d744bb376951f4db0a459fcf3c82
Lisa <kuchynaliza86@gmail.com>
2023-12-01 22:34:13 +0200

    adding logOut button

--- src/VIEW/UserProfileView.java
+++ src/VIEW/UserProfileView.java
@@ -1,9 +1,14 @@
 package VIEW;
 
 
+import INTERFACE_ADAPTER.logout_adapter.LogOutController;
+import INTERFACE_ADAPTER.view_event.ViewEventState;
 import INTERFACE_ADAPTER.view_profile.ViewProfileController;
 import INTERFACE_ADAPTER.view_profile.ViewProfileViewModel;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
@@ -15,11 +20,14 @@ public class UserProfileView extends VBox {
     private final Label hostedEventsLabel = new Label();
     private final Button logoutButton = new Button("Log Out");
 
+    private final LogOutController logOutController;
+
     private final ViewProfileViewModel viewProfileViewModel;
 
-    public UserProfileView(ViewProfileViewModel viewProfileViewModel, ViewProfileController viewProfileController) {
+    public UserProfileView(ViewProfileViewModel viewProfileViewModel, ViewProfileController viewProfileController, LogOutController logOutController) {
         this.viewProfileViewModel = viewProfileViewModel;
         this.viewProfileController = viewProfileController;
+        this.logOutController = logOutController;
 
         initUI();
     }
@@ -42,10 +50,21 @@ public class UserProfileView extends VBox {
         );
         setSpacing(5);
 
-//        logoutButton.setOnAction(event -> handleActionButtonClick());
-//        logoutButton.setOnAction(event -> {
-//          //////////////////
-//        });
+        logoutButton.setOnAction(event -> handleLogoutButtonClick());
+
+    }
+
+    private void handleLogoutButtonClick() {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to log out?", ButtonType.YES, ButtonType.NO);
+        alert.showAndWait();
+
+        if (alert.getResult() == ButtonType.YES) {
+            //logOutController.execute();
+
+            Platform.exit();
+            System.exit(0);
+        }
     }
 
     public void updateProfile() {
